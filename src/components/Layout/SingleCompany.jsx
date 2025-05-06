3f48d802f10b6c59feaf93edea416e44ff801786
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-05-06 23:30:45 +0600

    adding firebase config

--- src/components/Layout/SingleCompany.jsx
+++ src/components/Layout/SingleCompany.jsx
@@ -7,7 +7,7 @@ const SingleCompany = ({ company }) => {
   return (
     <Link to={`/company-details/${name.toLowerCase()}`}>
       <div className="px-6 py-4 cursor-pointer rounded-xl bg-[#ffffff15] space-y-2  hover:bg-gradient-to-br from-secondary hover:to-primary ">
-        <img src={logo} className="max-w-1/2  " alt="image" />
+        <img src={logo} className="max-w-1/2 min-h-[135px] " alt="image" />
         <h2 className="text-white text-3xl font-semibold">{name}</h2>
         <p className="text-accent font-semibold">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit.
